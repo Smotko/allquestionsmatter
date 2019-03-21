dda3bde288ed1d98aa06e5a5cabae20e0fed2657
Anže Pečar <anze@pecar.me>
2019-03-21 19:16:10 +0000

    Configure postgres

--- rooted/settings.py
+++ rooted/settings.py
@@ -76,9 +76,20 @@ WSGI_APPLICATION = 'rooted.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     'OPTIONS': {
+    #         'timeout': 100
+    #     }
+    # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "rooted",
+        "USER": "postgres",
+        "PASSWORD": "mypass",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
