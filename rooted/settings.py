f4a673427a643ba0f5f2a7c65f8f6d85890922f9
Anže Pečar <anze@pecar.me>
2019-03-22 18:26:31 +0000

    Disable debug in prod

--- rooted/settings.py
+++ rooted/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xwc-_hyh-l(&jnc-tmh8^#op8-%1=)rlplg2a*pqgryjyt54k5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "true") == "true"
 
 ALLOWED_HOSTS = []
 
